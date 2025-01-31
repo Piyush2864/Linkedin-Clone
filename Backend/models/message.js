1e664a84590641ce0d4ab9ebb6fe3e0bfd76949b
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2025-01-31 19:52:15 +0530

    message model added

---
+++ Backend/models/message.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const Message = sequelize.define('Message', {
+      sender_id: DataTypes.INTEGER,  
+      receiver_id: DataTypes.INTEGER, 
+      content: DataTypes.TEXT,        
+      is_read: {                      
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    });
+  
+    Message.associate = (models) => {
+      Message.belongsTo(models.User, { foreignKey: 'sender_id', as: 'sender' });
+      Message.belongsTo(models.User, { foreignKey: 'receiver_id', as: 'receiver' });
+    };
+  
+    return Message;
+  };
+  
