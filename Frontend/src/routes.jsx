b0fdd81fafa13f2966f8bcfb7c5f74828e35a926
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2025-02-02 23:08:26 +0530

    packages installed

---
+++ Frontend/src/routes.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
